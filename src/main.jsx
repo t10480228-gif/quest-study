b3aea736efcfa29ce97221e9ef1641a0dd7a058b
t10480228-gif <toshiya0228@gmail.com>
2026-07-23 17:23:01 +0900

    main.jsxのimportパスを修正

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./QuestStudy.jsx";
+import App from "../QuestStudy.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
